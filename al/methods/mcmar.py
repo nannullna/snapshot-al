ae1aee8a534bc5170d7f70dfd4f8db24207a14bf
nannullna <nannullna00@gmail.com>
2022-11-16 16:47:24 +0900

    fatal error

--- al/methods/mcmar.py
+++ al/methods/mcmar.py
@@ -2,10 +2,16 @@ from typing import List, Optional, Iterable
 
 import torch
 import torch.nn.functional as F
+
+from ..pool import ActivePool
 from .mcdropout import MCDropoutQuery
 
 
 class MCMarginSampling(MCDropoutQuery):
+
+    def __init__(self, model: torch.nn.Module, pool: ActivePool, size: int = 1, device: torch.device = None, **kwargs):
+        super().__init__(model, pool, size, device, **kwargs)
+        self.descending = False
 
     def _query_impl(self, batch_outs: torch.Tensor) -> List[float]:
         prob = F.softmax(batch_outs, dim=1) # [B, K, C]
